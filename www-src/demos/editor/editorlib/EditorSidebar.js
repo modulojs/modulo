c9c34c89d38878d49cf1c021b0e8e739c254672a
michaelb <michaelpb@gmail.com>
2023-01-25 09:09:01 -0800

    Fix yesno filter

--- www-src/demos/editor/editorlib/EditorSidebar.js
+++ www-src/demos/editor/editorlib/EditorSidebar.js
@@ -23,7 +23,7 @@ function initializedCallback () {
             name: "options",
             boxes: [
                 "<x-LayoutSettings></x-LayoutSettings>",
-                "<x-FileEditorSettings onchange:=script.onFileEditorOptionsChange></x-FileEditorSettings>",
+                "<x-FileEditorSettings></x-FileEditorSettings>",
             ],
             emoji: "🎨",
         },
@@ -31,15 +31,6 @@ function initializedCallback () {
     if (!ALLOW_NULL) {
         toggleTab('files');
     }
-}
-
-function onFileBrowserChange(payload) {
-    // NOTE: Dead code! FileBrowser will probably just be links...
-    console.log('onFileBrowserChange', payload);
-}
-
-function onFileEditorOptionsChange(setName, value) {
-    props.onoptionschange(setName, value);
 }
 
 function toggleSidebar () {
