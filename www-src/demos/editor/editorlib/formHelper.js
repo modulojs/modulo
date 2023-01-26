85e9a3d06df612acedd592937cb0a6fed4ca02cd
michaelb <michaelpb@gmail.com>
2023-01-26 09:57:59 -0800

    Modulo editor work

--- www-src/demos/editor/editorlib/formHelper.js
+++ www-src/demos/editor/editorlib/formHelper.js
@@ -1,23 +1,34 @@
 // Include this and use setValue to make a component behave a bit like an input
 // with a change event
+
+let isBool = null;
+
 function initializedCallback() {
     element.stateChangedCallback = (function (name, newValue, el) {
         this.value = newValue;
-        this.cparts.state.value = newValue; // ensure set
+        if (isBool) {
+            this.value = !!this.value;
+        }
+        this.cparts.state.value = this.value; // ensure set
         this.rerender();
     }).bind(element);
-    if (typeof state.value === 'boolean') {
-        state.value = props.value || '';
-        element.value = state.value;
-    } else {
-        state.value = props.value || '';
-        element.value = state.value;
+    if (isBool === null) {
+        isBool = typeof state.value === 'boolean';
     }
+    state.value = props.value || '';
+    if (isBool) {
+        state.value = !!state.value;
+    }
+    element.value = state.value;
 }
 
 function setValue(payload) {
-    element.value = payload;
-    state.value = payload;
+    if (isBool) {
+        element.value = !element.value;
+    } else {
+        element.value = payload;
+    }
+    state.value = element.value;
     element.dispatchEvent(new window.Event('change'));
 }
 
